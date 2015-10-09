18dfa6c6fc3a2841e07814718c7f439b2841cc47
Noah <noah@koontzs.com>
2015-10-09 11:19:46 -0700

    Porting to atmega32U4! (pro micro)

--- Port32U4.ino
+++ Port32U4.ino
@@ -1,6 +1,6 @@
 /* File automatically created by MariaMole */
 #define DEBUG
-#include "header.h"
+#include <DuinoTagV2.h>
 
 using namespace std;
 using namespace Sounds;
@@ -8,46 +8,46 @@ using namespace Sounds;
 Suit laser;
 IRrecv recver(4);  // I KNOW I SPELLED IT WRONG
 decode_results results;
-long int lastTime=0;
+long int lastTime = 0;
 
-void setup(){
-	#ifdef DEBUG
+void setup() {
+#ifdef DEBUG
 	Serial.begin(115200);
 	delay(200);
 	Serial.println("Booting up...");
-	#endif
+#endif
 	recver.enableIRIn();
-	#ifdef DEBUG
+#ifdef DEBUG
 	Serial.println("IR initilization done");
 	Serial.println("Entering setup mode");
-	#endif
+#endif
 	//laser.waitForSetup(&recver);
 	//pinMode(triggerPin, INPUT_PULLUP);
-	pinMode(13,OUTPUT); //used as game indicator during setup
-	laser.setup(0x02,0x10,&recver);
+	pinMode(13, OUTPUT); //used as game indicator during setup
+	laser.setup(0x02, 0x10, &recver);
 	//laser.startHealth=0x24;
-	#ifdef DEBUG
+#ifdef DEBUG
 	Serial.println("Suit setup done");
-	#endif
-	#ifdef DEBUG
+#endif
+#ifdef DEBUG
 	Serial.println("Starting game in 5 seconds...");
-	#endif
+#endif
 	digitalWrite(13, HIGH);
 	delay(5000);
 	digitalWrite(13, LOW);
-	laser.sCommand(cStartGame,0);
+	laser.sCommand(cStartGame, 0);
 }
 
 
-void loop(){
-	if(millis()-lastTime > 100){
-		if(!laser.gunCommand(gShoot,0)) laser.gunCommand(gReload,0);
-		#ifdef DEBUG
+void loop() {
+	if (millis() - lastTime > 100) {
+		if (!laser.gunCommand(gShoot, 0)) laser.gunCommand(gReload, 0);
+#ifdef DEBUG
 		Serial.println("BANG");
-		#endif
-		lastTime=millis();
+#endif
+		lastTime = millis();
 	}
-	if(laser.checkStatus()){
-		laser.sCommand(cRespawn,0);
+	if (laser.checkStatus()) {
+		laser.sCommand(cRespawn, 0);
 	}
 }
